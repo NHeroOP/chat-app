2f6293e41635491e3c51a0e9b663d51fdd92281d
NHeroOP <neeravtheheroo@gmail.com>
2024-04-20 23:24:35 +0530

    use theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter as FontSans } from "next/font/google" 
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,10 +21,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body className={cn("min-h-screen bg-black text-white font-sans antialiased", fontSans.variable)}>
+    <html lang="en">
+      <body className={cn("min-h-screen dark:bg-gray-950 bg-white text-white font-sans antialiased", fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
         <main>{children}</main>
         <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
