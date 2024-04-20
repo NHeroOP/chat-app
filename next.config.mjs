aad8c0563cc8a8f477258c9d5bb5e91188a7f9c4
NHeroOP <neeravtheheroo@gmail.com>
2024-04-20 23:26:16 +0530

    update these files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+  }
+};
 
 export default nextConfig;
