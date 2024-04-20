a748a7051f41ae66973c6d4b8082fc73f3e65a5b
NHeroOP <neeravtheheroo@gmail.com>
2024-04-20 23:25:23 +0530

    update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import Credentials from "next-auth/providers/credentials"
 import connectDB from "./lib/connectDB"
 import UserModel from "./model/User.model"
 import { compare } from "bcryptjs"
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
@@ -11,18 +11,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       name: "Credentials",
       credentials: {
         email: {label: "Email", type: "email"},
-        password: {label: "Password", type: "passsword"},
+        password: {label: "Password", type: "password"},
       },
       authorize: async (credentials:any): Promise<any> => {
         await connectDB()
-
+        
         try {
-          const user = await UserModel.findOne({
-            $or: [
-              {email: credentials.email},
-              {username: credentials.username},
-            ]
-          })
+          const user = await UserModel.findOne({email: credentials.email})
 
           if(!user) {
             throw new Error("User not found with this email")
@@ -32,7 +27,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             throw new Error("Please verify before login")
           }
 
-          const isPassCorrect = await compare(credentials.passsword, user.password)
+          const isPassCorrect = await compare(credentials.password, user.password)
 
           if(!isPassCorrect) {
             throw new Error("Incorrect Password")
@@ -44,28 +39,34 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           throw new Error(err)
         }
       },
+      
     }),
   ],
+  
   callbacks: {
     async jwt({ token, user }){
-      token._id = user._id
-      token.username = user.username
-      token.isVerified = user.isVerified
+      
+      if (user) {
+        token._id = user._id
+        token.username = user.username
+        token.isVerified = user.isVerified
+      }
+
       return token
     },
     async session({ session, token }){
-      session.user._id = token._id
-      session.user.username = token.username
-      session.user.isVerified = token.isVerified
-
+      session.user._id = token._id as string
+      session.user.username = token.username as string
+      session.user.isVerified = token.isVerified as boolean
+      
       return session
     },
   },
-  // pages: {
-  //   signIn: "/login",
-  // },
+  pages: {
+    signIn: "/login",
+  },
   session: {
     strategy: "jwt"
   },
-  secret: process.env.AUTH_SECRET
+  secret: process.env.AUTH_SECRET,
 })
